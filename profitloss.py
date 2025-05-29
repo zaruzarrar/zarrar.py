a166ba9b44a7205a55468e71472570cc8a28aa31
zaruzarrar <zarrararslan2012@gmail.com>
2025-05-29 15:33:14 +0500

    Add files via upload

---
+++ profitloss.py
@@ -0,0 +1,8 @@
+Cost=int(input("Enter the Cost price :"))
+sell=int(input("Enter the Selling price :"))
+result=Cost-sell
+if Cost>sell:
+    print("You got a losss of",result)
+else:
+        print("You got a profit of",result)
+
