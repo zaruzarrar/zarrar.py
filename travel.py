04f4b7aed9f1bee7e3e1c1b7f3f26d11e312b2a9
zaruzarrar <zarrararslan2012@gmail.com>
2025-06-05 16:27:17 +0500

    Add files via upload

---
+++ travel.py
@@ -0,0 +1,33 @@
+print("Enter your choice of vehicle")
+print("1.Car")
+print("2.Bike")
+choice= input("enter your choice:")
+if choice=="1":
+    print("You have choosed car")
+    print("Enter your car choice")
+    print("1.Suv")
+    print("2.Sudan")
+    choiceofcar=input("Enter your choice")
+    if  choiceofcar== "1":
+     print("Your cost is 40000")
+    else:
+      print("Your cost is 30000")
+
+else:
+     print("You have choosed bike")
+     print("Enter your bike choice")
+     print("1.Sports Bike")
+     print("2.Trail Bike")
+     choiceofbike=input("Enter your choice")
+     if   choiceofbike== "1":
+        print("Your cost is 20000")
+        print("Your cost is 15000")
+
+
+
+
+
+
+
+ 
+
