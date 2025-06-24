d9ad6d49621017cb9b72044fa302dbae3a5f7edb
zaruzarrar <zarrararslan2012@gmail.com>
2025-06-24 15:25:10 +0500

    Add files via upload

---
+++ debug.py
@@ -0,0 +1,22 @@
+# Test 1: Print numbers 1 to 10 using a for loop
+
+for i in range(1, 11):
+ 
+     print("Number:", i)
+
+if i % 2 == 0:
+
+    print("Even")
+
+else:
+  
+   print("Odd")
+ 
+# Test 2: Countdown from 10 to 1 using while loop
+
+j = 10
+
+while j >= 1:
+
+  print("Countdown:", j)
+  j = j - 1
